311e69b310d066970ab843a238e64fb4d326ba42
SlimFordy <magnoclifford42@gmail.com>
2025-04-19 20:18:43 +0800

    ipadayun ra nako ni unya kung d ko mahubog

--- testingLogIn/src/main/java/com/example/testingLogIn/CustomObjects/EvaluationStatus.java
+++ testingLogIn/src/main/java/com/example/testingLogIn/CustomObjects/EvaluationStatus.java
@@ -1,6 +1,7 @@
 package com.example.testingLogIn.CustomObjects;
 
 import com.example.testingLogIn.ModelDTO.UserDTO;
+import com.example.testingLogIn.Models.Section;
 import com.example.testingLogIn.Models.Subject;
 import com.example.testingLogIn.WebsiteSecurityConfiguration.UserModel;
 import lombok.AllArgsConstructor;
@@ -13,13 +14,15 @@ import lombok.NoArgsConstructor;
 public class EvaluationStatus {
     public UserDTO teacher;
     public Subject subject;
+    public Section section;
     public int gradedCount;
     public int totalToBeGraded;
 
-    public EvaluationStatus(UserModel teacher, Subject subject) {
+    public EvaluationStatus(UserModel teacher, Section section, Subject subject) {
         if(teacher != null){
             this.teacher = teacher.mapperDTO();
         }
+        this.section = section;
         this.subject = subject;
     }
 }
